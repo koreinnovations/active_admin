7cac81a641f9acdc8720ef1fa44ec57929ba0f12
Noah Lively <noah@koremedia.net>
2013-08-21 20:29:37 -0700

    jquery ui dependency fix

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -1,5 +1,7 @@
 //= require jquery
-//= require jquery-ui
+//= require jquery.ui.core
+//= require jquery.ui.widget
+//= require jquery.ui.datepicker
 //= require jquery_ujs
 
 //= require active_admin/application
